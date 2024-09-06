d2c144385c6bd86bea629f8ea76924ee0b43e421
Shivakumar-001 <shivagana2004@gmail.com>
2024-09-06 22:42:12 +0530

    Arithmetic Operators

---
+++ Basic Programs/Arithmetic.java
@@ -0,0 +1,12 @@
+
+class Arithmetic {
+
+    public static void main(String args[]) {
+        int a = 10, b = 3;
+        System.out.println("a + b =" + (a + b));
+        System.out.println("a - b =" + (a - b));
+        System.out.println("a * b =" + (a * b));
+        System.out.println("a / b =" + (a / b));
+        System.out.println("a % b =" + (a % b));
+    }
+}
