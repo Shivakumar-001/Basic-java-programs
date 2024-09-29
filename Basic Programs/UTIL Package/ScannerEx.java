5d7393e4fa60a885d1a743581c1bb0e881f8b77f
Shivakumar-001 <shivagana2004@gmail.com>
2024-09-29 22:50:40 +0530

    Java Programs

---
+++ Basic Programs/UTIL Package/ScannerEx.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ScannerEx
+{
+    public static void main(String args[])
+    {
+        System.out.println("Enter the number to read:");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println("Entered number is :+"n);
+    }
+}
+
+/*
+Output : Enter the number to read: 10
+        Entered number is 10
+ */ 
